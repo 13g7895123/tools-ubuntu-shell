a1f1a9b6d8407f2979410b0dd280e27ca3c2e523
Jarvis <13g7895123@gmail.com>
2023-03-24 17:34:44 +0800

    first commit

---
+++ building_winmai_env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Update
+sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
+
+# Lamp
+sudo apt install tasksel
+sudo tasksel install lamp-server
+
+sudo service apache2 start
+
+# Adminer
+sudo mkdir /usr/share/adminer
+sudo wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/latest.php
+sudo ln -s /usr/share/adminer/latest.php /usr/share/adminer/adminer.php
+echo "Alias /adminer.php /usr/share/adminer/adminer.php" | sudo tee /etc/apache2/conf-available/adminer.conf
+sudo a2enconf adminer.conf
+sudo systemctl reload apache2
+sudo a2enmod rewrite
+sudo service apache2 restart
