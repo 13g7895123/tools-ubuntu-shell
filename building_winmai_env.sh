9265b9fdd4932c769234d0262eac5c907d9f3e5b
Jarvis <13g7895123@gmail.com>
2023-03-24 18:52:19 +0800

    add 2nd part

--- building_winmai_env.sh
+++ building_winmai_env.sh
@@ -4,8 +4,8 @@
 sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
 
 # Lamp
-sudo apt install tasksel -y
-sudo tasksel install lamp-server
+sudo apt install -y tasksel
+sudo tasksel install -y lamp-server
 
 sudo service apache2 start
 
@@ -18,3 +18,17 @@ sudo a2enconf adminer.conf
 sudo systemctl reload apache2
 sudo a2enmod rewrite
 sudo service apache2 restart
+
+#PHP Packages
+sudo apt-get install -y php-mbstring
+sudo apt-get install -y php-intl
+sudo apt-get install -y php-curl 
+sudo apt-get install -y php-zip 
+sudo apt-get install -y php-ssh2
+sudo apt-get install -y phpmd
+sudo apt-get install -y php-mcrypt
+sudo apt-get install -y php-imagick
+sudo apt-get install -y php-simplexml
+sudo apt-get install -y php-xml
+sudo apt-get install -y php-gd
+sudo apt install -y gettext
---
+++ building_winmai_env_02.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#PHP Packages
+sudo apt-get install -y php-mbstring
+sudo apt-get install -y php-intl
+sudo apt-get install -y php-curl 
+sudo apt-get install -y php-zip 
+sudo apt-get install -y php-ssh2
+sudo apt-get install -y phpmd
+sudo apt-get install -y php-mcrypt
+sudo apt-get install -y php-imagick
+sudo apt-get install -y php-simplexml
+sudo apt-get install -y php-xml
+sudo apt-get install -y php-gd
+sudo apt install -y gettext
