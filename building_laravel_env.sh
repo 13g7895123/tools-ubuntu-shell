28b7392032530ec2c701e74d242f7f037aa15269
Jarvis <13g7895123@gmail.com>
2023-03-30 19:13:02 +0800

    add building laravel vue env -chatgpt

--- building_laravel_env.sh
+++ building_laravel_env.sh
@@ -1,33 +1,26 @@
 #!/bin/sh
 
-# Update package list and install LAMP stack
-sudo apt-get update -y
-sudo apt-get install -y lamp-server
-
-# Install Git
-sudo apt-get install -y git
-
 # Install Composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
-# Install Node.js and npm
+# Install PHP8.2
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+
+sudo apt update
+sudo apt install -y php8.2-fpm
+
+# Install PHP8.2 套件
+sudo apt-get install -y php8.2-xml
+sudo apt-get install -y php8.2-curl
+
+# Install apache
+sudo apt-get install -y apache2
+
+# Install Node.js
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
-sudo apt-get install -y npm
 
-# Install PHP extensions
-sudo apt-get install -y php-curl php-gd php-mbstring php-xml php-xmlrpc
-sudo service apache2 restart
+# Building Laravel project
+composer create-project --prefer-dist laravel/laravel:^9.0 laravel-vue-build-chatgpt
 
-#PHP Packages
-sudo apt-get install -y php-mbstring
-sudo apt-get install -y php-intl
-sudo apt-get install -y php-curl 
-sudo apt-get install -y php-zip 
-sudo apt-get install -y php-ssh2
-sudo apt-get install -y phpmd
-sudo apt-get install -y php-mcrypt
-sudo apt-get install -y php-imagick
-sudo apt-get install -y php-simplexml
-sudo apt-get install -y php-xml
-sudo apt-get install -y php-gd
-sudo apt install -y gettext
